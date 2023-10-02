fac60064621ef9d4c97ce4ed6a8044a60992aa08
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-10-02 14:33:26 +0300

    add

--- home.py
+++ home.py
@@ -1,7 +1,16 @@
 import sys
 from pathlib import Path
 
-def main():
+
+def sort_folder(path:Path, elements=[]) -> None:
+    for item in path.iterdir():
+        if item.is_dir():
+            sort_folder(item, elements)
+        else:
+            elements.append(item) 
+    print(elements)        
+
+def main() -> str: 
     try:
         path = Path(sys.argv[1])
     except IndexError:
@@ -9,6 +18,8 @@ def main():
                     
     if not path.exists():
         return "Folder dos not exists"
+    
+    sort_folder(path)
         
     return "All OK"
     
