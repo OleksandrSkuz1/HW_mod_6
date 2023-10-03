0812bf19406472a6e3513379e07641618140bf85
Oleksandr_Skuz <138138803+OleksandrSkuz1@users.noreply.github.com>
2023-10-03 11:34:30 +0300

    add

--- home.py
+++ home.py
@@ -1,27 +1,49 @@
 import sys
 from pathlib import Path
 
+CATEGORIES = {"Audio": [".mp3",],
+              "Video": [".mp4"],
+              "Fotos": [".jpg"],
+              "Docs": [".rtf", ".txt", ".bmp"]}
 
-def sort_folder(path:Path, elements=[]) -> None:
-    for item in path.iterdir():
-        if item.is_dir():
-            sort_folder(item, elements)
-        else:
-            elements.append(item) 
-    print(elements)        
+def get_categories(file:Path) -> str:
+    ext = file.suffix.lower()
+    for cat, exts in CATEGORIES.items():
+        if ext in exts:
+            return cat
+    return "Other"  
 
-def main() -> str: 
+def move_file(file:Path, category:str, root_dir:Path) -> None:
+    target_dir = root_dir.joinpath(category)
+    print(target_dir.exists())
+    if not target_dir.exists():
+        target_dir.mkdir()
+        
+    file.replace(target_dir.joinpath(file.name))    
+        
+    
+def sort_folder(path:Path) -> None:
+    
+    for element in path.glob("**/*"):
+        if element.is_file():
+            category = get_categories(element)
+            move_file(element, category, path)
+            
+        
+
+
+def main() -> str:
     try:
         path = Path(sys.argv[1])
     except IndexError:
         return "No path to folder"
-                    
+                
     if not path.exists():
         return "Folder dos not exists"
     
     sort_folder(path)
-        
-    return "All OK"
-    
+                
+    return "All ok"
+
 if __name__ == "__main__":
     print(main())
